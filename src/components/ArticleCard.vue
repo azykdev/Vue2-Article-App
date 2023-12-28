256f0eda4e2d33872a55007bd631014ef52981bc
azykdev <azyk9114@gmail.com>
2023-12-28 15:25:26 +0500

    #15 - Loader

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="col">
-    <div class="card shadow-sm">
-      <div class="card-header " style="background-color: #1f1f1f">
+    <div class="card border-0" style="font-family: 'Times New Roman', Times, serif;">
+      <div class="card-header " style="background-color: #b7b7b7">
         <h6 class="my-0 fw-light text-muted mb-3">{{ article.author.username }}</h6>
-        <h5 class="card-title text-light">{{ article.title }}</h5>
+        <h5 class="card-title text-dark">{{ article.title }}</h5>
       </div>
       <div class="card-body">
         <p class="card-text">{{ article.description }}</p>
         <div class="d-flex justify-content-between align-items-center">
           <div class="btn-group">
-            <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+            <button type="button" class="btn btn-sm btn-outline-secondary">Read more</button>
             <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
           </div>
-          <small class="text-body-secondary">9 mins</small>
+          <small class="text-body-secondary">{{ new Date(article.createdAt).toLocaleString() }}</small>
         </div>
       </div>
     </div>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,9 +21,13 @@ export default {
 
 <template>
   <div class="container">
-
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-      <ArticleCard v-for="article in articles" :key="article.id" :article="article" />
+    <div v-if="loading" class="mb-3 bg-light d-flex justify-content-center align-items-center" style="height: 80vh;">
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+    <div v-else class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+      <ArticleCard  v-for="article in articles" :key="article.id" :article="article" />
     </div>
   </div>
 </template>
