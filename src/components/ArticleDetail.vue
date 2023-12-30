6b600084100c17072de70cc7c6ded710b4434fbb
azykdev <azyk9114@gmail.com>
2023-12-30 16:36:29 +0500

    #19 - Article detail part-4 (UI)

--- src/components/ArticleDetail.vue
+++ src/components/ArticleDetail.vue
@@ -1,21 +1,54 @@
 <template>
-  <div>
-    {{ articleBySlug }}
+  <div class="overflow-hidden" v-if="articleBySlug">
+    <div class="row g-2">
+      <div class="col-12">
+        <div class="p-3 ">
+          <h2>{{ articleBySlug.title }}</h2>
+        </div>
+      </div>
+      <div class="col-md-8">
+        <div class="p-3 ">
+          <p class="text-muted">Description:</p>
+          <p class="card-text font-monospace" >{{ articleBySlug.description }}</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="p-3 ">
+          <p class="text-muted font-monospace">Created: {{ new Date(articleBySlug.createdAt).toLocaleString() }}</p>
+          <p class="text-muted m-0 font-monospace">Updated: {{ new Date(articleBySlug.updatedAt).toLocaleString() }}</p>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="p-3 ">
+          <p class="text-muted">Author:</p>
+          <div class="d-flex align-items-center">
+            <img src="https://via.placeholder.com/50" alt="" class="rounded-circle" style="width: 50px; height: 50px;">
+            <p class="card-text ms-2">{{ articleBySlug.author.username }}</p>
+          </div>
+          <p class="text-muted mt-3">Bio:</p>
+          <p class="card-text ms-2">{{ articleBySlug.author.bio }}</p>
+        </div>
+      </div>
+      <div class="col-md-8">
+        <div class="p-3 ">
+          <p class="text-muted">Content:</p>
+          <p class="card-text font-monospace" >{{ articleBySlug.body }}</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  import { mapState } from 'vuex';
-  export default {
-    name: "ArticleDetail",
-    computed: {
-      ...mapState({
-        articleBySlug: state => state.articles.articleBySlug
-      })
-    }
+import { mapState } from 'vuex';
+export default {
+  name: "ArticleDetail",
+  computed: {
+    ...mapState({
+      articleBySlug: state => state.articles.articleBySlug
+    })
   }
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,11 +2,11 @@
     <div class="container py-3">
         <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
             <RouterLink class="d-flex align-items-center text-dark text-decoration-none" :to="{ name: 'home' }">
-                <img src="../assets/logo.png" alt="logo" width="30" />
+                <img src="../assets/logo.png" alt="logo" width="30"/>
                 <div class="fs-4"><span>Article</span></div>
             </RouterLink> 
             
-            <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto">
+            <nav class="d-flex mt-2 mt-md-0 ms-md-auto ">
                 <template v-if="isLoggedIn">
                     <RouterLink class="me-3 py-2 text-dark text-decoration-none" :to="{ name: 'home' }">{{ user.username.toUpperCase() }}</RouterLink>
                     <a href="#" class="me-3 py-2 text-dark text-decoration-none" @click="logout">Logout</a>
